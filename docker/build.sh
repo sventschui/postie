74f9acd20a48f528281e8169a065c02e58f816b3
Sven Tschui <github@sventschui.ch>
2020-02-13 14:29:26 +0100

    Allow non-root users to override index.html

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 VERSION="$(cat ../cli/package.json | jq -r '.version')"
+BUILD_NO="${1:-1}"
 
 echo "VERSION=$VERSION"
 
-docker build . -t postiee/postie:$VERSION-1 --build-arg "POSTIE_VERSION=$VERSION" 
+docker build . -t postiee/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" 
 
