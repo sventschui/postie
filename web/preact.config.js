2801ccf41c47aeea79b307b984a8de00f1fb19ee
valentinhasler <valentin.hasler13@gmail.com>
2021-01-20 11:09:20 +0100

    remove console.log

--- web/preact.config.js
+++ web/preact.config.js
@@ -1,7 +1,6 @@
 export default (config, env, helpers) => {
     let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
     let babelConfig = rule.options;
-    console.log(babelConfig);
 
     babelConfig.plugins.push(require.resolve('styled-jsx/babel'));
     babelConfig.plugins.push(require.resolve('babel-plugin-graphql-tag'));
