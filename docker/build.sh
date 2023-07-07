377cd4a5c721e3b32c1d12c1d75d58471421f896
TheDadi <dario.difazio@axa-winterthur.ch>
2023-07-07 04:07:54 +0200

    add docker release

--- docker/build.sh
+++ docker/build.sh
@@ -5,4 +5,5 @@ BUILD_NO="${1:-1}"
 echo "VERSION=$VERSION"
 
 docker build . -t axahealth/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" --platform=linux/amd64
+docker push axahealth/postie:$VERSION-$BUILD_NO
 
