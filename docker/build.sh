eb1489b7ecc00f7afdbea5cbd0a47b3f2a30f179
TheDadi <dario.difazio@axa-winterthur.ch>
2023-07-07 04:12:47 +0200

    change default build no to -0

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="$(cat ../packages/cli/package.json | jq -r '.version')"
-BUILD_NO="${1:-1}"
+BUILD_NO="${1:-0}"
 echo "VERSION=$VERSION"
 
 docker build . -t axahealth/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" --push
