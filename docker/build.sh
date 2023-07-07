a68da2ea9f3a619696a279dd5e5c19db06725c17
TheDadi <dario.difazio@axa-winterthur.ch>
2023-07-07 04:32:17 +0200

    remove arm

--- docker/build.sh
+++ docker/build.sh
@@ -4,5 +4,5 @@ VERSION="$(cat ../packages/cli/package.json | jq -r '.version')"
 BUILD_NO="${1:-0}"
 echo "VERSION=$VERSION"
 
-docker build . -t axahealth/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" --push --platform linux/amd64,linux/arm64
+docker build . -t axahealth/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" --push
 
