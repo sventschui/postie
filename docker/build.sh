ed8f32565780b1f5c7b6ae12960a22e50242f268
TheDadi <dario.difazio@axa-winterthur.ch>
2023-07-07 04:10:27 +0200

    push image

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,5 @@ VERSION="$(cat ../packages/cli/package.json | jq -r '.version')"
 BUILD_NO="${1:-1}"
 echo "VERSION=$VERSION"
 
-docker build . -t axahealth/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" --platform=linux/amd64
-docker push axahealth/postie:$VERSION-$BUILD_NO
+docker build . -t axahealth/postie:$VERSION-$BUILD_NO --build-arg "POSTIE_VERSION=$VERSION" --push
 
